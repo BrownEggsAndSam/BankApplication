a3c89bfa8edb00f7554cc418c1e02611530b7fa4
Samuel Razia <78444732+BrownEggsAndSam@users.noreply.github.com>
2023-04-20 00:54:06 -0400

    Update account.py

--- account.py
+++ account.py
@@ -1,184 +1,57 @@
-import os
-import pandas as pd
-from datetime import datetime
-
-# Read input data
-print("Reading input data...")
-input_file = 'input_file.xlsx'
-input_data = pd.read_excel(input_file)
-
-# Consolidate EDG
-print("Consolidating EDG...")
-def consolidate_edg():
-    files = ['EDG.xlsx', 'NewSF.xlsx', 'NC.xlsx', 'InA.xlsx']
-    input_folder = os.path.join('_theStewardshipTool','input_data', 'Consolidated_EDG')
-
-    data_frames = []
-    for file in files:
-        file_path = os.path.join(input_folder, file)
-        df = pd.read_excel(file_path)
-        data_frames.append(df)
-
-    consolidated_EDG = pd.concat(data_frames, ignore_index=True)
-
-    # Save consolidated_EDG to an Excel file
-    output_folder = 'output'
-    os.makedirs(output_folder, exist_ok=True)
-    output_file = os.path.join(output_folder, 'consolidated_EDG.xlsx')
-    consolidated_EDG.to_excel(output_file, index=False)
-
-    print(f"Successfully consolidated EDG. Output saved to {output_file}")
-    
-    return consolidated_EDG
-
-consolidated_EDG = consolidate_edg()
-
-# DSET and PDD occurrences
-print("Processing DSET and PDD occurrences...")
-def dset_pdd_occurrences():
-    input_folder = os.path.join('_theStewardshipTool','input_data', 'DSET_PDD')
-    dset_path = os.path.join(input_folder, 'DSET.xlsx')
-    pdd_path = os.path.join(input_folder, 'PDD.xlsx')
-    dset = pd.read_excel(dset_path)
-    pdd = pd.read_excel(pdd_path)
-
-    merged = pd.concat([dset, pdd])
-    merged = merged[['Name', 'represented by [Business Term] > Full Name']]
-    merged.columns = ['Physical_Name', 'Attribute_ID']
-    merged['Occurrence_Count'] = merged.groupby(['Physical_Name', 'Attribute_ID'])['Physical_Name'].transform('count')
-
-    merged = merged.drop_duplicates()
-    merged.to_excel(os.path.join(input_folder, 'DSET_PDD_Occurrences.xlsx'), index=False)
-
-    return merged
-
-DSET_PDD_Occurrences = dset_pdd_occurrences()
-
-# Generate EDL Delta Hydration report
-def edl_delta_hydration(input_data):
-    print("Copying input data...")
-    reimport_data = input_data.copy()
-    reimport_data = reimport_data.applymap(lambda x: x.strip() if isinstance(x, str) else x)
-    reimport_data['Validation_Status'] = ''
-
-    # Add new columns to reimport_data DataFrame
-    reimport_data['represents Business Term [Business Term] > Name'] = ''
-    reimport_data['represents Business Term [Business Term] > Full Name'] = ''
-    reimport_data['represents Business Term [Business Term] > Asset Type'] = ''
-    reimport_data['represents Business Term [Business Term] > Community'] = ''
-    reimport_data['represents Business Term [Business Term] > Domain Type'] = ''
-    reimport_data['represents Business Term [Business Term] > Domain'] = ''
-
-    invalid_df = pd.DataFrame(columns=reimport_data.columns)
-    dupes_df = pd.DataFrame(columns=reimport_data.columns)
-    invalid_df['Replaced_ID'] = ''
-    invalid_df['Replaced_By_Name'] = ''
-
-    print("Starting data validation...")
-    for index, row in reimport_data.iterrows():
-        attr_id = row['Attribute Registry ID']
-        name = row['Name']
-        if pd.isnull(attr_id):
-            dset_pdd_matches = DSET_PDD_Occurrences[DSET_PDD_Occurrences['Physical_Name'] == name]
-            if len(dset_pdd_matches) == 1:
-                attr_id = dset_pdd_matches.iloc[0]['Attribute_ID']
-                reimport_data.at[index, 'Attribute Registry ID'] = attr_id
-            elif len(dset_pdd_matches) > 1:
-                dupes = []
-                for _, match in dset_pdd_matches.iterrows():
-                    match_attr_id = match['Attribute_ID']
-                    match_attr_name = consolidated_EDG.loc[consolidated_EDG['Full Name'] == match_attr_id, 'Name'].values[0]
-                    dupes.append(f"({match_attr_id}) - {match_attr_name}")
-                dupes_string = ";\n".join(dupes)
-                reimport_data.at[index, 'Attribute Registry ID'] = dupes_string
-                row['Attribute Registry ID'] = dupes_string  # Update the 'Attribute Registry ID' column in the dupes_df
-                row['Row_Number'] = index + 1
-                dupes_df = dupes_df.append(row)
-                reimport_data.at[index, 'Validation_Status'] = 'Check duplicate records sheet'
-                continue
-
-        if not pd.isnull(attr_id):
-            is_valid = (str(attr_id).startswith('ATTR') and str(attr_id)[4:].isdigit() and len(str(attr_id)) == 9) or (str(attr_id).isdigit() and len(str(attr_id)) == 5)
-        else:
-            continue
-
-        if not is_valid:
-            row['Row_Number'] = index + 1
-            invalid_df = invalid_df.append(row)
-            reimport_data.at[index, 'Validation_Status'] = 'Not Valid'
-            original_attr_id = row['Attribute Registry ID']
-
-            # Clear the record and try to see if there is a match using the look up with pdd_dset
-            reimport_data.at[index, 'Attribute Registry ID'] = ''
-            dset_pdd_matches = DSET_PDD_Occurrences[DSET_PDD_Occurrences['Physical_Name'] == name]
-            if len(dset_pdd_matches) == 1:
-                new_attr_id = dset_pdd_matches.iloc[0]['Attribute_ID']
-                reimport_data.at[index, 'Attribute Registry ID'] = new_attr_id
-                reimport_data.at[index, 'Validation_Status'] = 'Invalid replaced by tool'
-                # Append the new value it was replaced by
-                invalid_df.at[row['Row_Number'] - 1, 'Replaced_ID'] = new_attr_id
-                # Get the 'Name' of the 'Replaced_By' attribute from the consolidated EDG
-                replaced_by_name = consolidated_EDG.loc[consolidated_EDG['Full Name'] == new_attr_id, 'Name'].values[0]
-                invalid_df.at[row['Row_Number'] - 1, 'Replaced_By_Name'] = replaced_by_name
-            else:
-                reimport_data.at[index, 'Validation_Status'] = 'Not Valid'
-                invalid_df.at[row['Row_Number'] - 1, 'Replaced_ID'] = ''
-                invalid_df.at[row['Row_Number'] - 1, 'Replaced_By_Name'] = ''
-        else:
-            if reimport_data.at[index, 'Validation_Status'] != 'Invalid replaced by tool':
-                reimport_data.at[index, 'Validation_Status'] = 'Valid'
-
-        attr_id = reimport_data.at[index, 'Attribute Registry ID']
-        match = consolidated_EDG[consolidated_EDG['Full Name'] == attr_id]
-        if len(match) > 0:
-            reimport_data.at[index, 'represents Business Term [Business Term] > Name'] = match.iloc[0]['Name']
-            reimport_data.at[index, 'represents Business Term [Business Term] > Full Name'] = match.iloc[0]['Full Name']
-            reimport_data.at[index, 'represents Business Term [Business Term] > Asset Type'] = match.iloc[0]['Asset Type']
-            reimport_data.at[index, 'represents Business Term [Business Term] > Community'] = match.iloc[0]['Community']
-            reimport_data.at[index, 'represents Business Term [Business Term] > Domain Type'] = match.iloc[0]['Domain Type']
-            reimport_data.at[index, 'represents Business Term [Business Term] > Domain'] = match.iloc[0]['Domain']
-
-    # Save the output file
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    input_file_name = os.path.basename(input_file).split('.')[0]
-    output_folder = os.path.join(os.getcwd(), 'output', 'deltaHydration')
-    os.makedirs(output_folder, exist_ok=True)
-    output_file_path = os.path.join(output_folder, f"{input_file_name}_{timestamp}.xlsx")
-    reimport_data = reimport_data[~reimport_data['Validation_Status'].isin(['Check duplicate records sheet', '', 'Not Valid'])]
-    
-    print("Generating output file...")
-    with pd.ExcelWriter(output_file_path) as writer:
-        input_data.to_excel(writer, sheet_name="Original", index=False)
-        reimport_data.to_excel(writer, sheet_name="Reimport", index=False)
-
-        if not invalid_df.empty:
-            invalid_df.drop('Validation_Status', axis=1).to_excel(writer, sheet_name="Invalid Records", index=False)
-
-        if not dupes_df.empty:
-            dupes_df_mod = dupes_df.copy()
-            dupes_df_mod.drop(['Validation_Status'], axis=1).to_excel(writer, sheet_name="Dupes", index=False)
-
-    print("Output file generated successfully.")
-    return input_data, reimport_data
-
 # Call the edl_delta_hydration function
+# Function to generate the report
 def generate_report(input_data, reimport_data):
-    report = {}
+    original_records = len(input_data)
+    original_blanks = input_data['Attribute Registry ID'].isnull().sum()
+    original_invalids = len(input_data.loc[~input_data['Attribute Registry ID'].str.startswith("ATTR", na=False) & input_data['Attribute Registry ID'].notnull()])
+    original_valids = original_records - original_blanks - original_invalids
 
-    report['Original Records'] = len(input_data)
-    report['Blank Records (Original)'] = input_data['Attribute Registry ID'].isnull().sum()
-    report['Valid Attributes (Original)'] = input_data['Attribute Registry ID'].apply(lambda x: (str(x).startswith('ATTR') and str(x)[4:].isdigit() and len(str(x)) == 9) or (str(x).isdigit() and len(str(x)) == 5)).sum()
-    report['Invalid Attributes (Original)'] = report['Original Records'] - (report['Blank Records (Original)'] + report['Valid Attributes (Original)'])
+    reimport_records = len(reimport_data)
+    reimport_domain_counts = reimport_data['represents Business Term [Business Term] > Domain'].value_counts().to_dict()
+    reimport_invalids = len(reimport_data.loc[reimport_data['Validation_Status'] == 'Not Valid'])
+    reimport_replaced = len(reimport_data.loc[reimport_data['Validation_Status'] == 'Invalid replaced by tool'])
+    reimport_invalids_post_hydration = reimport_invalids - reimport_replaced
+    reimport_domain_counts = reimport_data['represents Business Term [Business Term] > Domain'].value_counts().to_dict()
 
-    report['Valid Attributes (Reimport)'] = (reimport_data['Validation_Status'] == 'Valid').sum()
-    report['Invalid Attributes'] = (reimport_data['Validation_Status'] == 'Not Valid').sum()
-    report['Invalid Replaced by Rehydration'] = (reimport_data['Validation_Status'] == 'Invalid replaced by tool').sum()
-    report['Exact Matches DSET_PDD'] = reimport_data['Attribute Registry ID'].apply(lambda x: (str(x).startswith('ATTR') and str(x)[4:].isdigit() and len(str(x)) == 9) or (str(x).isdigit() and len(str(x)) == 5)).sum() - report['Valid Attributes (Reimport)']
-    report['Duplicate Matches DSET_PDD'] = (reimport_data['Validation_Status'] == 'Check duplicate records sheet').sum()
-    report['Blank Records (Reimport)'] = reimport_data['Attribute Registry ID'].isnull().sum()
+    report = {
+        "Original Records": original_records,
+        "  Original Blanks": original_blanks,
+        "  Original Invalids": original_invalids,
+        "  Original Valids": original_valids,
+        "Reimport Records": reimport_records,
+        "Reimport Invalids Post Hydration": reimport_invalids_post_hydration
+    }
+    
+    for domain, count in reimport_domain_counts.items():
+        report[f"Reimport {domain} Count"] = count
 
     return report
 
+
+# Function to save the generated log report to a txt file
+def save_report_to_txt(report, output_folder, timestamp, input_file_creation_time, reimport_domain_counts):
+    log_file_name = f"delta_hydration_report_{timestamp}.txt"
+    log_file_path = os.path.join(output_folder, log_file_name)
+    
+    
+    with open(log_file_path, 'w') as log_file:
+        log_file.write(f"Input file creation time: {input_file_creation_time}\n\n")
+        
+        log_file.write("Pre-processed Metrics:\n")
+        for key, value in report.items():
+            if key.startswith("Original"):
+                log_file.write(f"{key}: {value}\n")
+        
+        log_file.write("\nHydration Metrics:\n")
+        for key, value in report.items():
+            if key.startswith("Reimport"):
+                log_file.write(f"{key}: {value}\n")
+                
+        log_file.write("\nDomain Breakdown:\n")
+        for domain, count in reimport_domain_counts.items():
+            log_file.write(f"  {domain}: {count}\n")        
+        
+        print(f"Log report saved to {log_file_path}")
 
 
 input_data, reimport_data = edl_delta_hydration(input_data)
@@ -187,3 +60,7 @@ report = generate_report(input_data, reimport_data)
 for key, value in report.items():
     print(f"{key}: {value}")
 
+
+# Call the save_report_to_txt function after generating the report
+input_file_creation_time = datetime.fromtimestamp(os.path.getctime(input_file)).strftime("%Y-%m-%d %H:%M:%S")
+output_folder = save_report_to_txt(report, output_folder, timestamp, input_file_creation_time)
